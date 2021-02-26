5e8b16ce4d9223fd520684d829fca4326dc2cd65
helloworldkim <arinwater@gmail.com>
2021-02-26 21:39:12 +0900

    설명하는 부분 /경로에 추가로 넣음 cors 관련해서 도메인주소도 추가로 넣어서 크로스오리진 제대로 되도록 변경함

--- react/src/App.js
+++ react/src/App.js
@@ -41,6 +41,10 @@ class App extends Component {
                 </li>
               </ul>
             </nav>
+            <div className="container">
+              <h1>React와 spring boot를 이용한 테스트 페이지입니다</h1>
+              <p className="p-1">기타 문의사항은 arinwater@gamil.com 으로 문의주시기 바랍니다.</p>
+            </div>
             <Switch>
               <Route exact path="/"></Route>
               <Route path="/login" component={LoginForm}></Route>
